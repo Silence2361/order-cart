c0c722160356f6b09a6272f5f2457431b2458087
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2024-12-11 16:21:24 +0300

    feat: added validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,17 @@
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsInt()
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   category: string;
+
+  @IsInt()
   stock: number;
 }
